8976a7637048fc8ad2d6f1fa29941eaffdefc8c6
Matthias Schmid, MSc <ramsondon@gmail.com>
2026-01-31 23:36:29 +0100

    replace img with Image component for favicons and add error handling in BookmarkCard

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,11 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'avatars.githubusercontent.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.google.com',
+        pathname: '/s2/favicons/**',
+      },
     ],
   },
 };
